c89af11c87769076bfe55e6f23afba9e976464d7
Jerry <jerrytekh134@users.noreply.github.com>
2026-09-24 10:26:45 +0100

    ci: fix CI checkout and commitlint for PR #447
    - Add fetch-depth: 0 to all checkout steps to ensure package-lock.json is fetched
    - Configure commitlint to ignore merge commits

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,25 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+        "ci",
+        "merge"
+      ]
+    ]
+  },
+  ignores: [(commit) => commit.type === "merge"]
 };
